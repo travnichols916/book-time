0e1b1aa30d170c65dcd598d3e690757319fa376e
travnichols916 <travnichols916@GMAIL.COM>
2022-07-06 15:06:23 -0700

    server and package json with new tech

---
+++ server/server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const {ApolloServer} = require('apollo-server-express');
+
+const path = require('path');
+
+const {typeDefs, resolvers} = require('./schemas');
+const db = require('./config/connection');
+// const routes = require('./routes');
+const { authMiddleware } = require('./utils/auth');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const startServer = async () => {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: authMiddleware
+  });
+  await server.start();
+  server.applyMiddleware({ app });
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`)
+}
+
+startServer()
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+// app.use(routes);
+
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on localhost:${PORT}`)}
+  );
+});
